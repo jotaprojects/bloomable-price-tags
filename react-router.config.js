69fe5bf830f5124f8371ef44cdcdc90a0cc789f4
jotaprojects <jotaprojects@gmail.com>
2025-01-20 20:35:34 +0100

    Update of basename to react-router config

--- react-router.config.js
+++ react-router.config.js
@@ -1,6 +1,7 @@
 export default {
   // Config options...
   appDirectory: "src",
+  basename: "/bloomable-price-tags/",
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
   prerender: true,
