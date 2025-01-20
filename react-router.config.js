ba9739b4df1331072b456adb489742071332f2e3
jotaprojects <jotaprojects@gmail.com>
2025-01-20 23:37:24 +0100

    Test add a home route

--- react-router.config.js
+++ react-router.config.js
@@ -1,7 +1,7 @@
 export default {
   // Config options...
   appDirectory: "src",
-  basename: "/bloomable-price-tags/",
+  basename: import.meta.env.DEV ? "/" : "/bloomable-price-tags/",
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
   prerender: true,
