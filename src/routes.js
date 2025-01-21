fb4f5493ecf89f7a47d73145905a3fcccffd6408
jotaprojects <jotaprojects@gmail.com>
2025-01-22 00:25:03 +0100

    Test of Netlify deployment

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,12 @@
 import { index, layout, route } from "@react-router/dev/routes";
 
 export default [
+  // layout("./layouts/rootLayout.jsx", [
+  //   route("bloomable-price-tags", "./routes/home.jsx", [
+  //     route("print", "./routes/plant/print.jsx"),
+  //     route("login", "./routes/login.jsx"),
+  //   ]),
+  // ]),
   layout("./layouts/rootLayout.jsx", [
     index("./routes/home.jsx"),
     route("print", "./routes/plant/print.jsx"),
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { reactRouter } from "@react-router/dev/vite";
-import autoprefixer from "autoprefixer";
-import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
+
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
-  plugins: [reactRouter()],
+  plugins: [reactRouter(), netlifyPlugin()],
   ssr: false,
   base: "/bloomable-price-tags/",
 });
