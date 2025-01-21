40d68b930e5313fbc5766de239f8dd5edf4aec02
jotaprojects <jotaprojects@gmail.com>
2025-01-21 12:34:09 +0100

    Revert "Removal of basename in react router config"
    This reverts commit 7ce83de20d3e0e1d5f1d45827317c437a335f763.

--- react-router.config.js
+++ react-router.config.js
@@ -1,6 +1,7 @@
 export default {
   // Config options...
   appDirectory: "src",
+  // basename: import.meta.env.DEV ? "/" : "/bloomable-price-tags/",
   basename: "/",
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
