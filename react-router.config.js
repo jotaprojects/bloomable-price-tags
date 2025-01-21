7ce83de20d3e0e1d5f1d45827317c437a335f763
jotaprojects <jotaprojects@gmail.com>
2025-01-21 12:26:52 +0100

    Removal of basename in react router config

--- react-router.config.js
+++ react-router.config.js
@@ -1,7 +1,6 @@
 export default {
   // Config options...
   appDirectory: "src",
-  // basename: import.meta.env.DEV ? "/" : "/bloomable-price-tags/",
   basename: "/",
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
